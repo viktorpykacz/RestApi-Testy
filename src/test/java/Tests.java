80098ae87c7b09a00ab4189341095dd12287f9e7
viktorpykacz <viktor.pykacz@gmail.com>
2024-04-14 12:32:09 +0200

    update

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -16,6 +16,7 @@ public class Tests extends TestListenerAdapter {
 
     @BeforeClass
     public static void setup() {
+        System.out.println("Starting_loading_configuration");
         String port = System.getProperty("server.port");
         if (port == null) {
             RestAssured.port = Integer.valueOf(5000);
@@ -35,71 +36,48 @@ public class Tests extends TestListenerAdapter {
             baseHost = "http://192.168.1.102";
         }
         RestAssured.baseURI = baseHost;
+        System.out.println("Finishing_loading_configuration");
     }
 
     @Test(groups={"Przychody"})
-    public void Check_If_Get_Przychody_Returns_200_OK()
+    public void Test_HealthCheck_Przychody_200_OK()
     {
+        System.out.println("Starting_Test_HealthCheck_Przychody_200_OK");
         given()
                 .when()
                 .get("/przychody")
                 .then()
                 .statusCode(200);
 
-        System.out.println("Check_If_Get_Przychody_Returns_200_OK");
+        System.out.println("Finishing_Test_HealthCheck_Przychody_200_OK");
     }
 
-    @Test(dependsOnGroups = "Przychody")
-    public void Check_If_Json_Przychody_Is_OK()
+    @Test(groups={"Przychody"})
+    public void Test_Add_Przychod()
     {
-        given()
-                .when()
-                .get("/przychody")
-                .then()
-                .body(containsString("dataWystawieniaFaktury"))
-                .body(containsString("terminPlatnosci"))
-                .body(containsString("numerFaktury"))
-                .body(containsString("nipKlienta"))
-                .body(containsString("opisFaktury"))
-                .body(containsString("wartoscNetto"))
-                .body(containsString("wartoscVat"))
-                .body(containsString("wartoscBrutto"));
-        System.out.println("Check_If_Json_Przychody_Is_OK");
-    }
-
-    @Test(dependsOnMethods = "Check_If_Json_Przychody_Is_OK" )
-    public void Check_If_Post_Przychody_Addes_New_Przychod()
-    {
+        System.out.println("Starting_Test_Add_Przychod");
         Map<String, String> Przychod = new HashMap<>();
         Przychod.put("dataWystawieniaFaktury","31.01.2024");
         Przychod.put("terminPlatnosci","07.02.2024");
         Przychod.put("numerFaktury","FV_TESTOWA_AUTOMAT");
         Przychod.put("nipKlienta","1132800777");
-        Przychod.put("opisFaktury","TESTOWA FAKTURA AUTOMAT TWORZENIE");
+        Przychod.put("opisFaktury","TESTOWY PRZYCHOD AUTOMAT TWORZENIE");
         Przychod.put("wartoscNetto","10000");
         Przychod.put("wartoscVat","2300");
         Przychod.put("wartoscBrutto","12300");
-
-        int IdPrzychod = given()
-                .contentType("application/json")
-                .body(Przychod)
-                .when()
-                .post("/przychody")
-                .then()
-                .statusCode(201)
-                .extract().path("id");
-        System.out.println("Testowy przychod dodany="+IdPrzychod);
+        System.out.println("Finishing_Test_Add_Przychod");
     }
 
-    @Test(dependsOnMethods = "Check_If_Json_Przychody_Is_OK")
-    public void Check_If_Post_Przychody_Addes_New_Przychod_And_Delete_Przychody_Deletes_Przychod()
+    @Test(groups={"Przychody"})
+    public void Test_Add_And_Delete_Przychod()
     {
+        System.out.println("Test_Add_And_Delete_Przychod");
         Map<String, String> Przychod = new HashMap<>();
         Przychod.put("dataWystawieniaFaktury","31.01.2024");
         Przychod.put("terminPlatnosci","07.02.2024");
         Przychod.put("numerFaktury","FV_TESTOWA_AUTOMAT");
         Przychod.put("nipKlienta","1132800777");
-        Przychod.put("opisFaktury","TESTOWA FAKTURA AUTOMAT TWORZENIE I KASOWANIE");
+        Przychod.put("opisFaktury","TESTOWY PRZYCHOD AUTOMAT TWORZENIE I KASOWANIE");
         Przychod.put("wartoscNetto","10000");
         Przychod.put("wartoscVat","2300");
         Przychod.put("wartoscBrutto","12300");
@@ -119,18 +97,20 @@ public class Tests extends TestListenerAdapter {
                 .when().delete("/przychody/{id}")
                 .then()
                 .statusCode(204);
-        System.out.println("Testowy przychod usuniety");
+        System.out.println("Testowy przychod "+IdPrzychod + " usuniety");
+        System.out.println("Finishing_Test_Add_And_Delete_Przychod");
     }
 
-    @Test(dependsOnMethods = "Check_If_Json_Przychody_Is_OK")
-    public void Check_If_Post_Przychody_Addes_New_Przychod_And_Put_Przychody_Updates_Przychod()
+    @Test(groups={"Przychody"})
+    public void Test_Add_And_Update_Przychod()
     {
+        System.out.println("Starting_Test_Add_And_Update_Przychod");
         Map<String, String> Przychod = new HashMap<>();
         Przychod.put("dataWystawieniaFaktury","31.01.2024");
         Przychod.put("terminPlatnosci","07.02.2024");
         Przychod.put("numerFaktury","FV_TESTOWA_AUTOMAT");
         Przychod.put("nipKlienta","1132800777");
-        Przychod.put("opisFaktury","TESTOWA FAKTURA AUTOMAT TWORZENIE");
+        Przychod.put("opisFaktury","TESTOWY PRZYCHOD AUTOMAT TWORZENIE");
         Przychod.put("wartoscNetto","10000");
         Przychod.put("wartoscVat","2300");
         Przychod.put("wartoscBrutto","12300");
@@ -152,7 +132,7 @@ public class Tests extends TestListenerAdapter {
         PrzychodUpdated.put("terminPlatnosci","01.02.2024");
         PrzychodUpdated.put("numerFaktury","FV_TESTOWA_AUTOMAT");
         PrzychodUpdated.put("nipKlienta","1132800777");
-        PrzychodUpdated.put("opisFaktury","TESTOWA FAKTURA AUTOMAT TWORZENIE_UPDATED");
+        PrzychodUpdated.put("opisFaktury","TESTOWY PRZYCHOD AUTOMAT TWORZENIE_UPDATED");
         PrzychodUpdated.put("wartoscNetto","20000");
         PrzychodUpdated.put("wartoscVat","4600");
         PrzychodUpdated.put("wartoscBrutto","24600");
@@ -164,11 +144,12 @@ public class Tests extends TestListenerAdapter {
                 .put("/przychody/"+IdPrzychod)
                 .then()
                 .statusCode(204);
-        System.out.println("Testowy przychod zaktualizowany");
+        System.out.println("Testowy przychod "+IdPrzychod + " zaktualizowany");
+        System.out.println("Finishing_Test_Add_And_Update_Przychod");
     }
 
     @Test(groups = {"Koszty"})
-    public void Check_If_Get_Koszty_Returns_200_OK()
+    public void HealthCheck_Koszty_200_OK()
     {
         given()
                 .when()
@@ -177,18 +158,30 @@ public class Tests extends TestListenerAdapter {
                 .statusCode(200);
     }
 
+    /*TODO
+    Test add koszty
+    Test add and delete koszty
+    Test add and update koszty
+     */
+
     @Test(groups={"GodzinyPracy"})
-    public void Check_If_Get_GodzinyPracy_Returns_200_OK()
+    public void HealthCheck_GodzinyPracy_200_OK()
     {
         given()
                 .when()
-                .get("/godzinyPracy")
+                .get("/GodzinyPracy")
                 .then()
                 .statusCode(200);
     }
 
+    /*TODO
+    Test add GodzinyPracy
+    Test add and delete GodzinyPracy
+    Test add and update GodzinyPracy
+     */
+
     @Test(groups = {"Kontrakty"})
-    public void Check_If_Get_Kontrakty_Returns_200_OK()
+    public void HealthCheck_Kontrakty_200_OK()
     {
         given()
                 .when()
@@ -197,18 +190,30 @@ public class Tests extends TestListenerAdapter {
                 .statusCode(200);
     }
 
+    /*TODO
+    Test add Kontrakty
+    Test add and delete Kontrakty
+    Test add and update Kontrakty
+     */
+
     @Test(groups = {"ZestawienieGodzinyPracy"})
-    public void Check_If_Get_ZestawienieGodzinyPracy_Returns_200_OK()
+    public void HealthCheck_ZestawienieGodzinyPracy_200_OK()
     {
         given()
                 .when()
-                .get("/zestawienieGodzinPracy")
+                .get("/ZestawienieGodzinPracy")
                 .then()
                 .statusCode(200);
     }
 
+    /*TODO
+    Test add ZestawienieGodzinyPracy
+    Test add and delete ZestawienieGodzinyPracy
+    Test add and update ZestawienieGodzinyPracy
+     */
+
     @Test(groups = {"ZestawienieMiesieczneFirmy"})
-    public void Check_If_Get_ZestawienieMiesieczneFirmy_Returns_200_OK()
+    public void HealthCheck_ZestawienieMiesieczneFirmy_200_OK()
     {
         given()
                 .when()
@@ -217,8 +222,14 @@ public class Tests extends TestListenerAdapter {
                 .statusCode(200);
     }
 
+    /*TODO
+    Test add ZestawienieMiesieczneFirmy
+    Test add and delete ZestawienieMiesieczneFirmy
+    Test add and update ZestawienieMiesieczneFirmy
+     */
+
     @Test(groups = {"Podatki"})
-    public void Check_If_Get_Podatki_Returns_200_OK()
+    public void HealthCheck_Podatki_200_OK()
     {
         given()
                 .when()
@@ -226,6 +237,12 @@ public class Tests extends TestListenerAdapter {
                 .then()
                 .statusCode(200);
     }
+
+    /*TODO
+    Test add podatki
+    Test add and delete podatki
+    Test add and update podatki
+     */
 
     /*TODO - add method to clean up the database from requests created by tests
     test
